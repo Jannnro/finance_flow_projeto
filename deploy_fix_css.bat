f518e8be6328fd4bfa04e37cdf5103f06a3c9fcf
Janelson Ortega <janelsonortega@gmail.com>
2025-11-26 16:48:36 -0300

    Fix: CSS syntax error in Dashboard.module.css

--- deploy_fix_css.bat
+++ deploy_fix_css.bat
@@ -1,4 +1,4 @@
 @echo off
 "C:\Program Files\Git\cmd\git.exe" add .
-"C:\Program Files\Git\cmd\git.exe" commit -m "Feat: Mobile responsiveness optimization"
+"C:\Program Files\Git\cmd\git.exe" commit -m "Fix: CSS syntax error in Dashboard.module.css"
 "C:\Program Files\Git\cmd\git.exe" push origin main
