c3f05d8cb3e708383dc6b519acf67e717dd9fd6b
Janelson Ortega <janelsonortega@gmail.com>
2025-12-03 15:11:57 -0300

    Fix: CSS syntax error in Dashboard.module.css

--- deploy_fix_css_build.bat
+++ deploy_fix_css_build.bat
@@ -1,4 +1,4 @@
 @echo off
 "C:\Program Files\Git\cmd\git.exe" add .
-"C:\Program Files\Git\cmd\git.exe" commit -m "Feat: Add monthly transaction filter"
+"C:\Program Files\Git\cmd\git.exe" commit -m "Fix: CSS syntax error in Dashboard.module.css"
 "C:\Program Files\Git\cmd\git.exe" push origin main
