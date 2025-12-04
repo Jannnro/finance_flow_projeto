aa8574d73faab60050a517fbff5f0aea2a5072dd
Janelson Ortega <janelsonortega@gmail.com>
2025-12-04 17:17:49 -0300

    Fix: Fully revert SummaryCard to simple layout

---
+++ deploy_final_revert.bat
@@ -0,0 +1,4 @@
+@echo off
+"C:\Program Files\Git\cmd\git.exe" add .
+"C:\Program Files\Git\cmd\git.exe" commit -m "Fix: Fully revert SummaryCard to simple layout"
+"C:\Program Files\Git\cmd\git.exe" push origin main
