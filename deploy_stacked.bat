82762b3dde6ead364bf626a64447376472185864
Janelson Ortega <janelsonortega@gmail.com>
2025-12-04 17:05:14 -0300

    Refactor: Stacked layout for bi-weekly balance

--- deploy_stacked.bat
+++ deploy_stacked.bat
@@ -1,4 +1,4 @@
 @echo off
 "C:\Program Files\Git\cmd\git.exe" add .
-"C:\Program Files\Git\cmd\git.exe" commit -m "Feat: Add bi-weekly balance split (Day 15/30)"
+"C:\Program Files\Git\cmd\git.exe" commit -m "Refactor: Stacked layout for bi-weekly balance"
 "C:\Program Files\Git\cmd\git.exe" push origin main
